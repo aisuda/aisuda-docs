f1dbb50d1d787d38b8da0de7c1841ae84472315e
wuduoyi <wuduoyi@baidu.com>
2021-05-13 17:38:38 +0800

    增加导出 pdf 功能

---
+++ .docsifytopdfrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pathToStatic: 'pdfstatic',
+  contents: ['_sidebar.md'], // array of "table of contents" files path
+  pathToPublic: 'doc.pdf', // path where pdf will stored
+  pdfOptions: '', // reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagepdfoptions
+  removeTemp: true, // remove generated .md and .html or not
+  emulateMedia: 'screen' // mediaType, emulating by puppeteer for rendering pdf, 'print' by default (reference: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pageemulatemediamediatype)
+};
